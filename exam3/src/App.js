7e624760a1715cb874942a442219f5500172cb61
cmjiwa <wangjinghuan@yahoo.com>
2022-09-14 01:17:31 +0300

    now can create a div display id upon clicking

--- exam3/src/App.js
+++ exam3/src/App.js
@@ -1,13 +1,29 @@
 import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
 function Aa(props) {
+  function clickHandler(e) {
+    e.preventDefault();
+    const id = e.target.id;
+    console.log("id = ", id);
+    const cc = document.createElement("div");
+    cc.innerText = `Content of section ${id}`;
+    const bb = document.querySelector(".tabs");
+    bb.appendChild(cc);
+  }
+
   console.log("props  = ", props);
   return (
     <div className="tabs">
       {props.children.map((child, index) => {
         console.log("child.props.title = ", child.props.title);
         return (
-          <button className="btn" key={index} id={index}>
+          <button
+            className="btn"
+            key={index}
+            id={index + 1}
+            onClick={clickHandler}
+          >
             {child.props.title}
           </button>
         );
