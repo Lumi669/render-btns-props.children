1fa0a7f6ca4b8bd124a1bd43d5ada2adf49cf3c9
cmjiwa <wangjinghuan@yahoo.com>
2022-09-15 20:04:30 +0300

    Get rid of debugging codes

--- exam3/src/App.js
+++ exam3/src/App.js
@@ -1,60 +1,44 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
-
 function Aa(props) {
   const { children } = props;
 
-  //const [isDisabled, setIsDisabled] = useState(false);
-
+  //create view element for the clicking button
   function createE(id) {
-    //   // if (!fort) {
     const cc = document.createElement("div");
     cc.setAttribute("id", `content${id}`);
     cc.className = "view";
     cc.innerText = `Content of section ${Number(id) + 1}`;
     const bb = document.querySelector(".tabs");
     bb.append(cc);
-    //   // }
   }
 
   function clickHandler(e) {
     e.preventDefault();
     const id = e.target.id;
-    console.log("id = ", id);
     const currentClickedBtn = document.getElementById(id);
     currentClickedBtn.setAttribute("disabled", "");
 
     createE(id);
 
+    //get all the button elements
     const allBtnElements = document.getElementsByClassName("btn"); //htmlcollection
     const allBtnElementsArray = Array.from(allBtnElements); //array
-    console.log("sssaaaa = ", allBtnElementsArray);
-    allBtnElementsArray.forEach((btnElement) => {
-      console.log("btn element id = ", btnElement.id);
 
+    //check if not the current clicking button, then remove disabled and remove its corresponding view element
+    allBtnElementsArray.forEach((btnElement) => {
       if (btnElement.id !== id) {
         btnElement.removeAttribute("disabled");
-        const aa = `content${btnElement.id}`;
-        console.log("aa = ", aa);
-        const viewElement = document.getElementById(aa);
+        const idOfViewElement = `content${btnElement.id}`;
+        const viewElement = document.getElementById(idOfViewElement);
         if (viewElement !== null) {
           viewElement.remove();
         }
-        console.log("ppp = ", viewElement);
-        // viewElement.remove();
       }
-
-      // if (btnElement.id !== id) {
-      //   // btnElement.removeAttribute("disabled");
-      //   currentClickedBtn.setAttribute("disabled", false);
     });
   }
 
-  // console.log("props  = ", props);
   return (
     <div className="tabs">
       {props.children.map((child, index) => {
-        // console.log("child.props.title = ", child.props.title);
         let element = (
           <button className="btn" key={index} id={index} onClick={clickHandler}>
             {child.props.title}
