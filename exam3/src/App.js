7a2403d2547ad2f3d7747683fa2c5e31c5f1971d
cmjiwa <wangjinghuan@yahoo.com>
2022-09-16 21:42:00 +0300

    Now use React.Children.map, easier to write, so commented out some codes

--- exam3/src/App.js
+++ exam3/src/App.js
@@ -4,12 +4,12 @@ function Aa(props) {
   const { children } = props;
 
   //convert children to array
-  const childrenArray = React.Children.toArray(children);
+  //const childrenArray = React.Children.toArray(children);
 
   //only keep title as the key of child.props
-  const validChildren = childrenArray.filter(
-    (child) => child.props.hasOwnProperty("title") === true
-  );
+  // const validChildren = childrenArray.filter(
+  //   (child) => child.props.hasOwnProperty("title") === true
+  // );
 
   //create view ('view' is the className) element for the clicking button
   function createE(id) {
@@ -52,22 +52,19 @@ function Aa(props) {
 
   return (
     <div className="tabs">
-      {validChildren &&
-        validChildren.map((validChild, index) => {
-          console.log("child = ", validChild);
-          let element = (
-            <button
-              className="btn"
-              key={index}
-              id={index}
-              onClick={clickHandler}
-            >
-              {validChild.props.title}
-            </button>
-          );
+      {React.Children.map(children, (validChild, index) => {
+        console.log("child = ", validChild);
+        let element = (
+          <button className="btn" key={index} id={index} onClick={clickHandler}>
+            {validChild.props.title}
+          </button>
+        );
+        let retElement;
 
-          return element;
-        })}
+        retElement = validChild.props.title ? element : <></>;
+
+        return retElement;
+      })}
     </div>
   );
 }
@@ -75,12 +72,22 @@ function Aa(props) {
 const App = () => {
   return (
     <Aa>
-      <button titl="a1" />
-      <button titl="a2" />
-      <button titl="a3" />
+      <button title="a1" />
+      <button title="a2" />
+      <button title="a3" />
       <button title="a4" />
     </Aa>
   );
 };
 
 export default App;
+
+/**
+ * 
+ Summary of using React.Children.map
+ 1. import React from 'react', Line1
+ 2. destructure const {children} = props, Line 4
+ 3. React.Children.map(children, (child) => {child xxx}), Line55
+
+ 
+ */
